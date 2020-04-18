30340b4a548cf96b7944b38ea66f87e98072f355
keithZmudzinski <kzmudz79@gmail.com>
2020-04-18 16:07:33 -0700

    Updated info(3) test

--- Lab1/lab1_test.c
+++ Lab1/lab1_test.c
@@ -3,9 +3,8 @@
 #include "user.h"
 
 int main(int argc, char** argv) {
-    int value, value1, value2, value3, value4, value5, value6 = 0;
+    int value, value1, value2, value3, value4, value5 = 0;
     int pid = 0;
-    void* mem;
 
     // Test info(1)
     value = info(1);
@@ -25,10 +24,8 @@ int main(int argc, char** argv) {
 
     // Test info(3)
     value3 = info(3);
-    mem = malloc(24);
+    malloc(24);
     value4 = info(3);
-    free(mem);
-    value6 = info(3);
 
     // Output info(1) info
     printf(1, "Called info(1), got %d processes.\nCalled fork, and then info(1) again from parent process.\n\
@@ -40,8 +37,6 @@ Got %d processes the second time.\n\n", value, value5);
 
     // Output info(3) info
     printf(1, "Called info(3), got %d pages.\nThen called malloc(24), \
-and called info(3) again.\nGot %d pages the second time.\n\
-Then called free(), and info(3) again.\n\
-Got %d pages the third time.\n", value3, value4, value6);
+and called info(3) again.\nGot %d pages the second time.\n", value3, value4);
     exit();
 }
